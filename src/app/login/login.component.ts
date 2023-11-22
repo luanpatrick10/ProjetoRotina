b5eb5b1e32860dac7a50989188fceefe0e762b30
Luan <luanpck@hotmail.com>
2023-11-22 01:15:39 -0300

    Terminando ideia inicial login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,11 +5,12 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, MatSlideToggleModule,MatIconModule,MatButtonModule,MatInputModule,MatFormFieldModule],
+  imports: [CommonModule, MatSlideToggleModule,MatIconModule,MatButtonModule,MatInputModule,MatFormFieldModule,MatCheckboxModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
