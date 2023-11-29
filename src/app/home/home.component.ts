77e6ca6ff79d0d0114584ca70c56e1e45224e2ea
Luan <luanpck@hotmail.com>
2023-11-29 01:46:08 -0300

    Iniciado página home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,MatSidenavModule,MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  constructor(private _formBuilder: FormBuilder) {}
+  showFiller = false;
+  options = this._formBuilder.group({
+    bottom: 0,
+    fixed: false,
+    top: 0,
+  });
 }
